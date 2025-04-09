d6082541e7e49e1a7f49e70048111931ee07045c
sukanya0512 <sukanyamulimani05@gmail.com>
2025-04-09 09:59:41 +0530

    save

---
+++ sub.java
@@ -0,0 +1,9 @@
+public class sub {
+      public static void main(String[] args) {
+            int a = 10;
+            int b = 5;
+            int sub = a - b;
+            System.out.println("subtraction of two numbers is :" + sub);
+
+      }
+}
